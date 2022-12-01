2d1c722f51ba5338cc4b17fd54ecdbe6efe7aee6
Elena <lenka.penka123412@gmail,com>
2022-12-01 22:26:07 +0300

    Работа с рандомными числами new Random().Next

---
+++ Task5/Program.cs
@@ -0,0 +1,6 @@
+﻿int a = new Random().Next(1, 10); // случайное число 1,2,3,4,5...9
+Console.WriteLine(a);
+int b = new Random().Next(1, 10);
+Console.WriteLine(b);
+int result = a + b;
+Console.WriteLine(result);
